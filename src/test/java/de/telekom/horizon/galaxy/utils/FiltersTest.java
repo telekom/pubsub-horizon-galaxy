69b40d4d1d89940eed8c41d5ee66aa7498305439
Karsten Bruhns <karsten.bruhns@telekom.de>
2024-02-22 10:49:26 +0100

    test: add test for responseFilter

---
+++ src/test/java/de/telekom/horizon/galaxy/utils/FiltersTest.java
@@ -0,0 +1,43 @@
+package de.telekom.horizon.galaxy.utils;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.telekom.eni.pandora.horizon.kubernetes.resource.SubscriptionTrigger;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FiltersTest {
+    @Test
+    void includeFilterShouldOnlyIncludeFields() throws URISyntaxException, IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        JsonNode payload = new ObjectMapper().readTree(new File(getClass().getClassLoader().getResource("response-filter-test-data/payload.json").toURI()));
+        Method method = Filters.class.getDeclaredMethod("applyResponseFilter", List.class, SubscriptionTrigger.ResponseFilterMode.class, JsonNode.class);
+        method.setAccessible(true);
+
+        JsonNode response = (JsonNode) method.invoke(null, List.of("customer.number", "total"), SubscriptionTrigger.ResponseFilterMode.INCLUDE, payload);
+        JsonNode expectedOutput = new ObjectMapper().readTree(new File(getClass().getClassLoader().getResource("response-filter-test-data/payload-include-expected-output.json").toURI()));
+
+        assertEquals(expectedOutput, response);
+
+    }
+
+    @Test
+    void excludeFilterShouldOnlyExcludeFields() throws URISyntaxException, IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        JsonNode payload = new ObjectMapper().readTree(new File(getClass().getClassLoader().getResource("response-filter-test-data/payload.json").toURI()));
+        Method method = Filters.class.getDeclaredMethod("applyResponseFilter", List.class, SubscriptionTrigger.ResponseFilterMode.class, JsonNode.class);
+        method.setAccessible(true);
+
+        JsonNode response = (JsonNode) method.invoke(null, List.of("customer.number", "total"), SubscriptionTrigger.ResponseFilterMode.EXCLUDE, payload);
+        JsonNode expectedOutput = new ObjectMapper().readTree(new File(getClass().getClassLoader().getResource("response-filter-test-data/payload-exclude-expected-output.json").toURI()));
+
+        assertEquals(expectedOutput, response);
+    }
+}
