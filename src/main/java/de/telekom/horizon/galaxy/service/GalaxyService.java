50ac8bde9b947c4a571ad2f3c6c5023f50a54234
Mike Herwig <Mike.Herwig@telekom.de>
2025-01-24 14:46:47 +0100

    feat: streamlined shutdown handling

--- src/main/java/de/telekom/horizon/galaxy/service/GalaxyService.java
+++ src/main/java/de/telekom/horizon/galaxy/service/GalaxyService.java
@@ -4,9 +4,9 @@
 
 package de.telekom.horizon.galaxy.service;
 
-import de.telekom.horizon.galaxy.StopGalaxyServiceEvent;
 import de.telekom.horizon.galaxy.config.GalaxyConfig;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -44,32 +44,54 @@ public class GalaxyService {
     }
 
     /**
-     * Handles the requested termination of Horizon Galaxy service
+     * Will be invoked when the bean should be destroyed due to an application shutdown
+     *
+     * Stops the message listener container and therefore the further processing of messages
+     *
      */
-    @EventListener
-    public void onStopService(StopGalaxyServiceEvent event) {
+    @PreDestroy
+    public void stopService() {
+        // stop message listener container
         if (messageListenerContainer != null && messageListenerContainer.isRunning()) {
-            log.warn("Stopping MessageListenerContainer. Reason: {}", event.getMessage());
+            log.warn("Stopping MessageListenerContainer due to application shutdown.");
             messageListenerContainer.stop();
         }
     }
 
     /**
-     * Will be invoked when the message listener container stopped.
-     * If the application is not shutting down already, exit immediately.
+     * Will be invoked when the message listener container stopped
+     *
+     * Initiates the graceful shutdown of the application
+     *
      */
     @EventListener(value = {ContainerStoppedEvent.class})
     public void containerStoppedHandler() {
-        if (!context.isClosed()) {
-            log.error("MessageListenerContainer stopped unexpectedly. Exiting in {} seconds...", config.getShutdownWaitTimeSeconds());
-            // wait for some time for ongoing message processing to finish
-            try {
-                Thread.sleep(Instant.ofEpochSecond(config.getShutdownWaitTimeSeconds()).toEpochMilli());
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
+        gracefulShutdown();
+    }
 
-            System.exit(SpringApplication.exit(context, () -> 1));
+    /**
+     * Handles the graceful shutdown of the application
+     *
+     * The method checks whether the application's context already has been closed. Depending on the outcome
+     * the shutdown will be handled either as expected or unexpected.
+     *
+     */
+    private void gracefulShutdown() {
+        var isContextClosed = context.isClosed();
+
+        if (isContextClosed) {
+            log.warn("MessageListenerContainer stopped. Exiting application in {} seconds...", config.getShutdownWaitTimeSeconds());
+        } else {
+            log.error("MessageListenerContainer stopped unexpectedly. Exiting application in {} seconds...", config.getShutdownWaitTimeSeconds());
         }
+
+        // wait for some time for ongoing tasks to finish
+        try {
+            Thread.sleep(Instant.ofEpochSecond(config.getShutdownWaitTimeSeconds()).toEpochMilli());
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.exit(SpringApplication.exit(context, () -> isContextClosed ? 0 : 1));
     }
 }
