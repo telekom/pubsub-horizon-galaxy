f96198078b9d784374412fa594cb4bf893e9d50f
Jens Fischer <jens-fischer@telekom.de>
2026-01-20 09:46:58 +0100

    test(filtering): add additional test case for @ prefixed fields

--- src/test/java/de/telekom/horizon/galaxy/filters/JsonPathFiltersTest.java
+++ src/test/java/de/telekom/horizon/galaxy/filters/JsonPathFiltersTest.java
@@ -33,8 +33,10 @@ class JsonPathFiltersTest {
                     "hello": "world",
                     "foo": {
                         "bar": "fizz",
-                        "the-cake": "is a lie"
+                        "the-cake": "is a lie",
+                        "@prefixed": "at-prefixed"
                     },
+                    "@prefixed": "foobar",
                     "buzz": [
                         {
                             "value": 42
@@ -56,11 +58,13 @@ class JsonPathFiltersTest {
 
         // Include
         {
-            var includeJsonNode = JsonPathFilters.applyJsonPathResponseFilter(List.of("$.hello", "$.foo.bar", "$.buzz[?(@.value == 21)]", "$.numbers[?(@ < 3)]"), ResponseFilterMode.INCLUDE, jsonNode.deepCopy());
+            var includeJsonNode = JsonPathFilters.applyJsonPathResponseFilter(List.of("$.hello", "$.foo.bar", "@prefixed", "foo.@prefixed", "$.buzz[?(@.value == 21)]", "$.numbers[?(@ < 3)]"), ResponseFilterMode.INCLUDE, jsonNode.deepCopy());
             assertFalse(includeJsonNode.at("/hello").isMissingNode());
             assertFalse(includeJsonNode.at("/foo/bar").isMissingNode());
             assertTrue(includeJsonNode.at("/foo/the-cake").isMissingNode());
             assertFalse(includeJsonNode.at("/buzz").isMissingNode());
+            assertFalse(includeJsonNode.at("/foo/@prefixed").isMissingNode());
+            assertFalse(includeJsonNode.at("/@prefixed").isMissingNode());
 
             var filteredBuzz = Lists.newArrayList(includeJsonNode.at("/buzz").elements());
             assertEquals(1, filteredBuzz.size());
@@ -75,9 +79,11 @@ class JsonPathFiltersTest {
 
         // Exclude
         {
-            var excludeJsonNode = JsonPathFilters.applyJsonPathResponseFilter(List.of("$.buzz[?(@.value == 21)]", "$.numbers[?(@ < 3)]"), ResponseFilterMode.EXCLUDE, jsonNode.deepCopy());
+            var excludeJsonNode = JsonPathFilters.applyJsonPathResponseFilter(List.of("$.buzz[?(@.value == 21)]", "$.numbers[?(@ < 3)]", "foo.@prefixed"), ResponseFilterMode.EXCLUDE, jsonNode.deepCopy());
             assertTrue(excludeJsonNode.at("/buzz/1").isMissingNode());
             assertFalse(excludeJsonNode.at("/buzz/0").isMissingNode());
+            assertTrue(excludeJsonNode.at("/foo/@prefixed").isMissingNode());
+            assertTrue(excludeJsonNode.at("/@prefixed").isMissingNode());
 
             List<Integer> remainingNumbers = new ArrayList<>();
             excludeJsonNode.at("/numbers").elements().forEachRemaining(v -> remainingNumbers.add(v.asInt()));
